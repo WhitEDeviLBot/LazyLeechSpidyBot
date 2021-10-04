d878ba8735ffb9a24a46c056d5ca143cb60be76d
mallumovieleech <88672268+mallumovieleech@users.noreply.github.com>
2021-10-04 14:08:44 +0530

    Update thumbnail.py

--- lazyleech/plugins/thumbnail.py
+++ lazyleech/plugins/thumbnail.py
@@ -20,7 +20,7 @@ from pyrogram import Client, filters
 from .. import ALL_CHATS, help_dict
 from ..utils.misc import convert_to_jpg, get_file_mimetype, watermark_photo
 
-@Client.on_message(filters.command(['thumbnail', 'savethumbnail', 'setthumbnail']) & filters.chat(ALL_CHATS))
+@Client.on_message(filters.command(['savethumbnail@MMLeechv5_bot', 'savethumbnail@MMLeechv5_bot', 'savethumbnail@MMLeechv5_bot']) & filters.chat(ALL_CHATS))
 async def savethumbnail(client, message):
     reply = message.reply_to_message
     document = message.document
@@ -57,7 +57,7 @@ async def savethumbnail(client, message):
     else:
         await message.reply_text('Cannot find thumbnail')
 
-@Client.on_message(filters.command(['clearthumbnail', 'rmthumbnail', 'delthumbnail', 'removethumbnail', 'deletethumbnail']) & filters.chat(ALL_CHATS))
+@Client.on_message(filters.command(['clearthumbnail@MMLeechv5_bot', 'rmthumbnail', 'delthumbnail', 'removethumbnail', 'deletethumbnail']) & filters.chat(ALL_CHATS))
 async def rmthumbnail(client, message):
     for path in ('thumbnail', 'watermarked_thumbnail'):
         path = os.path.join(str(message.from_user.id), f'{path}.jpg')
@@ -66,11 +66,11 @@ async def rmthumbnail(client, message):
     await message.reply_text('Thumbnail cleared')
 
 help_dict['thumbnail'] = ('Thumbnail',
-'''/thumbnail <i>&lt;as reply to image or as a caption&gt;</i>
-/setthumbnail <i>&lt;as reply to image or as a caption&gt;</i>
-/savethumbnail <i>&lt;as reply to image or as a caption&gt;</i>
+'''/savethumbnail@MMLeechv5_bot <i>&lt;as reply to image or as a caption&gt;</i>
+/savethumbnail@MMLeechv5_bot <i>&lt;as reply to image or as a caption&gt;</i>
+/savethumbnail@MMLeechv5_bot <i>&lt;as reply to image or as a caption&gt;</i>
 
-/clearthumbnail
+/clearthumbnail@MMLeechv5_bot
 /rmthumbnail
 /removethumbnail
 /delthumbnail
