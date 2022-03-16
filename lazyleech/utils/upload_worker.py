0ffd91c0af2c78448355b48646825641a14c8d14
DREAM CATCHER <63229691+DreamCatcher96@users.noreply.github.com>
2022-03-16 14:18:33 +0300

    Update upload_worker.py

--- lazyleech/utils/upload_worker.py
+++ lazyleech/utils/upload_worker.py
@@ -143,7 +143,7 @@ async def _upload_file(client, message, reply, filename, filepath, force_documen
     user_watermark = os.path.join(str(user_id), 'watermark.jpg')
     user_watermarked_thumbnail = os.path.join(str(user_id), 'watermarked_thumbnail.jpg')
     file_has_big = os.path.getsize(filepath) > 2097152000
-    upload_wait = await reply.reply_text(f'Upload of {html.escape(filename)} will start in {PROGRESS_UPDATE_DELAY}s')
+    upload_wait = await reply.reply_text(f'<b>Your Request {html.escape(filename)} Will Start in {PROGRESS_UPDATE_DELAY}s</b>')
     message_exists[upload_wait.chat.id].add(upload_wait.message_id)
     upload_identifier = (upload_wait.chat.id, upload_wait.message_id)
     async with upload_tamper_lock:
@@ -180,7 +180,7 @@ async def _upload_file(client, message, reply, filename, filepath, force_documen
                     if a:
                         async with upload_tamper_lock:
                             upload_waits.pop(upload_identifier)
-                            upload_wait = await reply.reply_text(f'Upload of {html.escape(filename)} will start in {PROGRESS_UPDATE_DELAY}s')
+                            upload_wait = await reply.reply_text(f'<b>Your Request {html.escape(filename)} Will Start in {PROGRESS_UPDATE_DELAY}s</b>')
                             upload_identifier = (upload_wait.chat.id, upload_wait.message_id)
                             upload_waits[upload_identifier] = user_id, worker_identifier
                         for _ in range(PROGRESS_UPDATE_DELAY):
